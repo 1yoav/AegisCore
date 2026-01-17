3e2b563f0d65145dcedeb4437abbc62aa420aa50
yiftah30 <iftacha@pelech.ort.org.il>
2026-01-17 20:03:54 +0200

    communicition with hooking working, still need to test with the signature scanner

--- mainAnalyze.py
+++ mainAnalyze.py
@@ -3,66 +3,58 @@ import win32pipe, win32file, pywintypes
 from threading import Thread
 
 
+hooking_data = {}
+
 def pipe_client_hooking():
-    print("pipe client")
+    pipe_path = r"\\.\\pipe\\hooking"
     quit = False
 
-    while not quit:
-        try:
-            handle = win32file.CreateFile(
-                r"\\.\pipe\hooking",
-                win32file.GENERIC_READ | win32file.GENERIC_WRITE,
-                0,
-                None,
-                win32file.OPEN_EXISTING,
-                0,
-                None
-            )
-            res = win32pipe.SetNamedPipeHandleState(handle, win32pipe.PIPE_READMODE_MESSAGE, None, None)
-            if res == 0:
-                print(f"SetNamedPipeHandleState return code: {res}")
-            while True:
-                resp = win32file.ReadFile(handle, 1024)
-                print(f"message: {resp}")
+    pipe = win32pipe.CreateNamedPipe(
+        pipe_path,
+        win32pipe.PIPE_ACCESS_DUPLEX,
+        win32pipe.PIPE_TYPE_MESSAGE
+        | win32pipe.PIPE_READMODE_MESSAGE
+        | win32pipe.PIPE_WAIT,
+        1,
+        65536,
+        65536,
+        0,
+        None
+    )
 
-        except pywintypes.error as e: #handling errors
-            if e.args[0] == 2:
-                print("no pipe, trying again in a sec")
-                time.sleep(1)
-            elif e.args[0] == 109:
-                print("broken pipe, bye bye")
-                quit = True
+    while quit == False:
+        win32pipe.ConnectNamedPipe(pipe, None)
+        res = (win32file.ReadFile(pipe, 200))
+
+        # just to see if working, in real case need to start action
+        print(res)
+        win32pipe.DisconnectNamedPipe(pipe)
+
 
 
 def pipe_client_signature_scanner():
-    print("pipe client")
+    pipe_path = "\\.\\pipe\\signatureScanner"
     quit = False
 
-    while not quit:
-        try:
-            handle = win32file.CreateFile(
-                r"\\.\pipe\signatureScanner",
-                win32file.GENERIC_READ | win32file.GENERIC_WRITE,
-                0,
-                None,
-                win32file.OPEN_EXISTING,
-                0,
-                None
-            )
-            res = win32pipe.SetNamedPipeHandleState(handle, win32pipe.PIPE_READMODE_MESSAGE, None, None)
-            if res == 0:
-                print(f"SetNamedPipeHandleState return code: {res}")
-            while True:
-                resp = win32file.ReadFile(handle, 1024)
-                print(f"message: {resp}")
+    pipe = win32pipe.CreateNamedPipe(
+        pipe_path,
+        win32pipe.PIPE_ACCESS_DUPLEX,
+        win32pipe.PIPE_TYPE_MESSAGE
+        | win32pipe.PIPE_READMODE_MESSAGE
+        | win32pipe.PIPE_WAIT,
+        1,
+        65536,
+        65536,
+        0,
+        None
+    )
+    win32pipe.ConnectNamedPipe(pipe, None)
 
-        except pywintypes.error as e: #handling errors
-            if e.args[0] == 2:
-                print("no pipe, trying again in a sec")
-                time.sleep(1)
-            elif e.args[0] == 109:
-                print("broken pipe, bye bye")
-                quit = True
+    while quit == False:
+        res = (win32file.ReadFile(pipe, 200)).decode()
+
+        # just to see if working, in real case need to start action
+        print(res)
 
 
 if __name__ == '__main__':
@@ -74,5 +66,3 @@ if __name__ == '__main__':
 
 
 
-    else:
-        print(f"no can do: {sys.argv[1]}")
